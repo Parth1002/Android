3f87d11dc9a05903a1b3c01cab35f0a5ee26a6c8
parth <parthfonfa@gmail.com>
2021-10-17 12:58:27 +0530

    Toast

--- app/src/main/java/com/example/pratical/MainActivity.java
+++ app/src/main/java/com/example/pratical/MainActivity.java
@@ -24,72 +24,7 @@ public class MainActivity extends AppCompatActivity {
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                 // Create the object of
-                // AlertDialog Builder class
-
-                AlertDialog.Builder builder
-                        = new AlertDialog
-                        .Builder(MainActivity.this);
-
-                // Set the message show for the Alert time
-                builder.setMessage("Do you want to exit ?");
-
-                // Set Alert Title
-                builder.setTitle("Alert !");
-
-                // Set Cancelable false
-                // for when the user clicks on the outside
-                // the Dialog Box then it will remain show
-                builder.setCancelable(false);
-
-                // Set the positive button with yes name
-                // OnClickListener method is use of
-                // DialogInterface interface.
-
-                builder
-                        .setPositiveButton(
-                                "Yes",
-                                new DialogInterface
-                                        .OnClickListener() {
-
-                                    @Override
-                                    public void onClick(DialogInterface dialog,
-                                                        int which)
-                                    {
-
-                                        // When the user click yes button
-                                        // then app will close
-                                        finish();
-                                    }
-                                });
-
-                // Set the Negative button with No name
-                // OnClickListener method is use
-                // of DialogInterface interface.
-                builder
-                        .setNegativeButton(
-                                "No",
-                                new DialogInterface
-                                        .OnClickListener() {
-
-                                    @Override
-                                    public void onClick(DialogInterface dialog,
-                                                        int which)
-                                    {
-
-                                        // If user click no
-                                        // then dialog box is canceled.
-                                        dialog.cancel();
-                                    }
-                                });
-
-                // Create the Alert dialog
-                AlertDialog alertDialog = builder.create();
-
-                // Show the Alert Dialog box
-                alertDialog.show();
-
+                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
             }
 
         });
