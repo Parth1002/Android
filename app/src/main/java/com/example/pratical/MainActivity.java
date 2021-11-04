92110f1ac7ee8dffd5886a1136cb989cc47b7717
parth <parthfonfa@gmail.com>
2021-11-04 11:57:02 +0530

    Insert database in firebase

--- app/src/main/java/com/example/pratical/MainActivity.java
+++ app/src/main/java/com/example/pratical/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.pratical;
 
 /**
- *  Radio Button in Textbox view
+ *
  */
+
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,21 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onRadioChecked(View view) {
-            boolean checked = ((RadioButton) view).isChecked();
+        final EditText txtNotice=findViewById(R.id.txtnotice);
+        Button btnclick=findViewById(R.id.btnclick);
 
-            // Check which radio button was clicked
-            switch(view.getId()) {
-                case R.id.Male:
-                    if (checked)
-                        Toast.makeText(MainActivity.this, "M", Toast.LENGTH_SHORT).show();
-                        break;
-                case R.id.Female:
-                    if (checked)
-                        Toast.makeText(MainActivity.this, "F", Toast.LENGTH_SHORT).show();
-                        break;
+        DAOEmployee dao= new DAOEmployee();
+
+        btnclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Employee emp = new Employee(txtNotice.getText().toString());
+                dao.add(emp).addOnSuccessListener(suc->{
+                    Toast.makeText(MainActivity.this, "Insert Database", Toast.LENGTH_SHORT).show();
+                }).addOnFailureListener(er->{
+                    Toast.makeText(MainActivity.this, ""+er.getMessage(), Toast.LENGTH_SHORT).show();
+                });
             }
-        }
+        });
     }
+}
