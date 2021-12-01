3c1f36f49599e5cfa99b8291c0cc4308044486cf
parth <parthfonfa@gmail.com>
2021-12-01 10:51:13 +0530

    ListView

--- app/src/main/java/com/example/pratical/MainActivity.java
+++ app/src/main/java/com/example/pratical/MainActivity.java
@@ -7,34 +7,22 @@ package com.example.pratical;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    String Arr[]={"Jay Goga","Jay Umiya Maa","Surekha","Parth"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listview);
 
-        final EditText txtNotice=findViewById(R.id.txtnotice);
-        Button btnclick=findViewById(R.id.btnclick);
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1,Arr);
+        listView.setAdapter(ad);
 
-        DAOEmployee dao= new DAOEmployee();
-
-        btnclick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Employee emp = new Employee(txtNotice.getText().toString());
-
-                    dao.add(emp).addOnSuccessListener(suc -> {
-                        Toast.makeText(MainActivity.this, "Insert Database", Toast.LENGTH_SHORT).show();
-                    }).addOnFailureListener(er -> {
-                        Toast.makeText(MainActivity.this, "" + er.getMessage(), Toast.LENGTH_SHORT).show();
-                    });
-                }
-        });
     }
 }
