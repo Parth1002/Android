d9503af1add79142911d571cd0f10cb57c58be04
parth <parthfonfa@gmail.com>
2022-04-09 17:34:40 +0530

    Open URL system

--- app/src/main/java/com/example/pratical/MainActivity.java
+++ app/src/main/java/com/example/pratical/MainActivity.java
@@ -1,50 +1,48 @@
 package com.example.pratical;
 
 /**
- * LinearLayout And CheckBox
+ *
  */
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    CheckBox CKGOOGLE;
-    CheckBox CKYAHOO;
-    CheckBox CKBING;
-    Button Button;
-    TextView TextView;
+    EditText TextUrl;
+    Button btnClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CKGOOGLE=findViewById(R.id.CKGOOGLE);
-        CKYAHOO=findViewById(R.id.CKYAHOO);
-        CKBING=findViewById(R.id.CKBING);
-        Button=findViewById(R.id.button);
-        TextView=findViewById(R.id.textView2);
+        TextUrl=findViewById(R.id.TextUrl);
+        btnClick=findViewById(R.id.btnClick);
 
-        Button.setOnClickListener(new View.OnClickListener() {
+        btnClick.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this, TextUrl.getText().toString(), Toast.LENGTH_SHORT).show();
+                Log.d("click", "onClick: open url");
 
-                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
-                if(CKGOOGLE.isChecked()){
-                    TextView.setText("YOUR SELECTED GOOGLE");
-                }
-                if(CKYAHOO.isChecked()) {
-                    TextView.setText("YOUR SELECTED YAHOO");
-                }
-                if(CKBING.isChecked()){
-                    TextView.setText("YOUR SELECTED BING");
+                Uri webpage=Uri.parse(TextUrl.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_VIEW,webpage);
+
+                if(intent.resolveActivity(MainActivity.this.getPackageManager())!=null){
+                    startActivity(intent);
                 }
             }
         });
