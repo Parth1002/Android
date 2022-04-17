57a656b57b2d9f53eb459c9f39889d803b235a33
parth <parthfonfa@gmail.com>
2022-04-17 17:23:50 +0530

    ScrollView App

--- app/src/main/java/com/example/pratical/MainActivity.java
+++ app/src/main/java/com/example/pratical/MainActivity.java
@@ -6,46 +6,23 @@ package com.example.pratical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Queue;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText TextUrl;
-    Button btnClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextUrl=findViewById(R.id.TextUrl);
-        btnClick=findViewById(R.id.btnClick);
-
-        btnClick.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, TextUrl.getText().toString(), Toast.LENGTH_SHORT).show();
-                Log.d("click", "onClick: open url");
-
-                Uri webpage=Uri.parse(TextUrl.getText().toString());
-                Intent intent = new Intent(Intent.ACTION_VIEW,webpage);
-
-                if(intent.resolveActivity(MainActivity.this.getPackageManager())!=null){
-                    startActivity(intent);
-                }
-            }
-        });
 
     }
 }
