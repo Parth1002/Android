bf7ed8528239e6913f1dbb9b8d2c0f1ac8edeec1
Parth1002 <parthfonfa@gmail.com>
2023-09-30 21:17:10 +0530

    Android 2
    Android 2

--- ParsingJSONData/app/src/main/java/com/example/parth/parsingjsondata/MainActivity.java
+++ ParsingJSONData/app/src/main/java/com/example/parth/parsingjsondata/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.parth.parsingjsondata;
 
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 'https://jsonplaceholder.typicode.com/todos/1'
+
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos/1", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("Myapp", "onCreate: The response is :- " + response.getString("title"));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Myapp", "onErrorResponse: Something went Wrong");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
